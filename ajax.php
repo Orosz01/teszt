3913b2f29b29b7fd7c705f9e7f2ce37b22dd9fd4
Orosz01 <gazsi1@hotmail.com>
2022-02-10 12:47:23 +0100

    biztonságos ajax

--- ajax.php
+++ ajax.php
@@ -2,16 +2,17 @@
 
 require 'includes/db.inc.php';
 
+$stmt=mysqli_prepare($conn,"SELECT nev FROM ulesrend WHERE nev LIKE ?");
 if(isset($_REQUEST['keres'])){
-   $sql="SELECT nev FROM ulesrend WHERE nev LIKE ('%".$_REQUEST['keres']."%')";
-    if($result = $conn-> query($sql)){
+   $stmt->bind_param('s', $nev); 
+   $nev = "%".$_REQUEST['keres']."%";
+   $stmt->execute();
+    if($result = $stmt->get_result()){
         if ($result->num_rows > 0 ){
             while($row = $result->fetch_assoc()){
                  echo $row['nev']."<br>";
             }
-          
-    } else echo "nem szerepel az adatbazisban";
-
-}
+    } else echo "Nincs benne az adatbázisban";
+    }   
 }
 ?>
